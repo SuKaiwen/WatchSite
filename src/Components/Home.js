13badf02c3bc4510924cce6862276b4f7c08be0c
Kevin Su <kevinsu@outlook.co.nz>
2021-09-10 14:49:53 +1200

    navbar and home 80% done

---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="parallax" style={{fontFamily:"Urbanist", margin:"auto", color:"white"}}>
+        <h1 className="center">Unlock time</h1>
+        <h4 className="center">Watches crafted from exquisite elements of nature</h4>
+        <div className="triple">
+            <h1>Yo</h1>
+            <h1>Yo</h1>
+            <h1>Yo</h1>
+        </div>
+    </div>
+  );
+}
+
+export default Home;
